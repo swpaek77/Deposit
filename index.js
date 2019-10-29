3b31488a5046af90d2203359c91814d22d445948
Seungwoo Paek <swpaek77@gmail.com>
2019-10-29 11:20:21 +0900

    1.0.0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var moment = require('moment');
 
 var app = express();
-var port = process.env.PORT || 7500;
+var port = process.env.PORT || 7000;
 
 app.use(express.json());
 app.set('view engine', 'ejs');
